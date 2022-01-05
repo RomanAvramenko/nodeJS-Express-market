74ed607e2cc0a0d36fa0909f47a50822eeeb10d8
RomanAvramenko <ashkaell24@gmail.com>
2022-01-05 12:11:28 +0200

    add file middleware and ability to change avatar

--- routes/profile.js
+++ routes/profile.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const auth = require("../middleware/auth");
+const User = require("../models/user");
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   res.render("profile", {
     title: "Profile",
     isProfile: true,
@@ -10,6 +11,24 @@ router.get("/", async (req, res) => {
   });
 });
 
-router.post("/", (req, res) => {});
+router.post("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    const toChange = {
+      name: req.body.name,
+    };
+
+    if (req.file) {
+      toChange.avatarURL = req.file.path;
+    }
+
+    Object.assign(user, toChange);
+    await user.save();
+    res.redirect("/profile");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
